fbe23e78601295c3ce5bbe1263d7b61f7dc6ea1a
Tommy McNeely <thomas.mcneely@davita.com>
2018-02-08 17:35:52 +0000

    change service provider

--- manifests/service.pp
+++ manifests/service.pp
@@ -4,8 +4,14 @@
 class aixldap::service {
 
   service { 'secldapclntd':
-    ensure => $aixldap::service_ensure,
-    enable => $aixldap::service_enable,
+    ensure   => $aixldap::service_ensure,
+    enable   => $aixldap::service_enable,
+    start    => '/usr/sbin/start-secldapclntd',
+    stop     => '/usr/sbin/stop-secldapclntd',
+    restart  => '/usr/sbin/restart-secldapclntd',
+    status   => '/usr/sbin/ls-secldapclntd',
+    provider => 'base',
+
   }
 
 }
